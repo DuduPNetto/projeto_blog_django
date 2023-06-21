9ccf98b0632b20f6a2e8d4dcb05fb371c8ef7d1b
DuduPNetto <dudupnetto.en@gmail.com>
2023-06-21 20:05:32 -0300

    Adding django_summernotes to post content

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -3,6 +3,7 @@ from typing import Any
 from blog import models
 from django.contrib import admin
 from django.http.request import HttpRequest
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(models.Tag)
@@ -41,7 +42,8 @@ class PageAdmin(admin.ModelAdmin):
     }
 
 @admin.register(models.Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = ('id', 'title', 'is_published',)
     list_display_links = ('title',)
     search_fields = ('id', 'title', 'slug', 'content', 'excerpt', 'cover',)
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,9 +1,12 @@
+from blog import models
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
 posts = list(range(1000))
 
 def index(request):
+    posts = models.Post.objects.order_by('-pk')
+
     paginator = Paginator(posts, 9)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
