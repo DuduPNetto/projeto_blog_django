9bfbce436311ab15bef56b37d1bdfca2e3d7960c
DuduPNetto <dudupnetto.en@gmail.com>
2023-06-22 13:51:14 -0300

    Creating Page page

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,5 +1,6 @@
 from blog import models
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 
 PER_PAGE = 9
@@ -64,10 +65,32 @@ def tag(request, slug):
         }
     )
 
-def page(request, slug):
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = models.Post.objects.get_published().filter(
+        Q(title__icontains=search_value) |
+        Q(excerpt__icontains=search_value) |
+        Q(content__icontains=search_value)
+    )[:PER_PAGE]
+
     return render(
         request,
-        'blog/pages/page.html'
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value
+        }
+    )
+
+def page(request, slug):
+    page = models.Page.objects.filter(is_published=True).filter(slug=slug).first()
+
+    return render(
+        request,
+        'blog/pages/page.html',
+        {
+            'page': page
+        }
     )
 
 def post(request, slug):
