33ad6551674a65e43139a395f2685a4079b4bb72
DuduPNetto <dudupnetto.en@gmail.com>
2023-06-22 21:42:08 -0300

    Creating Class Based View page

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.http import Http404, HttpRequest, HttpResponse
 from django.http.request import HttpRequest
 from django.shortcuts import redirect, render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 PER_PAGE = 9
 
@@ -127,22 +127,25 @@ class SearchListView(PostListView):
             return redirect('blog:index')
         return super().get(request, *args, **kwargs)
 
-def page(request, slug):
-    page_obj = models.Page.objects.filter(is_published=True).filter(slug=slug).first()
+class PageDetailView(DetailView):
+    model = models.Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
 
-    if page_obj is None:
-        raise Http404()
-
-    page_title = f'{page_obj.title} - Page - '
-
-    return render(
-        request,
-        'blog/pages/page.html',
-        {
-            'page': page_obj,
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        page = self.get_object()
+        page_title = f'{page.title} - Página - ' #type: ignore
+        context.update({
             'page_title': page_title
-        }
-    )
+        })
+        return context
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            is_published=True
+        )
 
 def post(request, slug):
     post_obj = models.Post.objects.get_published().filter(slug=slug).first()
